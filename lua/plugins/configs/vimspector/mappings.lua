84309a99261aa84aebc87cb34fabaeeea4cf1cad
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2021-11-03 18:56:36 +0000

    fixing vimspector mappings

--- lua/plugins/configs/vimspector/mappings.lua
+++ lua/plugins/configs/vimspector/mappings.lua
@@ -1,10 +1,5 @@
 require("utils")
 
-local function GoToWindow(id):
-    call win_gotoid(a:id)
-    MaximizerToggle
-end
-
 map("n", "<leader>dl", "<Plug>VimspectorStepInto", {})
 map("n", "<leader>dj", "<Plug>VimspectorStepOver", {})
 map("n", "<leader>dk", "<Plug>VimspectorStepOut", {})
@@ -13,11 +8,6 @@ map("n", "<leader>d<space>", ":call vimspector#Continue()<CR>", { noremap = true
 map("n", "<leader>drc", "<Plug>VimspectorRunToCursor", {})
 map("n", "<leader>dbp", "<Plug>VimspectorToggleBreakpoint", {})
 
-map("n", "<leader>m", ":MaximizerToggle!<CR>", { nnoremap = true })
-map("n", "<leader>dd", ":call vimspector#Launch()", { nnoremap = true })
-map("n", "<leader>dc", ":call GotoWindow(g:vimspector_session_windows.code)<CR>", { nnoremap = true })
-map("n", "<leader>dt", ":call GotoWindow(g:vimspector_session_windows.tabpage)<CR>", { nnoremap = true })
-map("n", "<leader>dv", ":call GotoWindow(g:vimspector_session_windows.variables)<CR>", { nnoremap = true })
-map("n", "<leader>do", ":call GotoWindow(g:vimspector_session_windows.output)<CR>", { nnoremap = true })
-map("n", "<leader>de", ":call vimspector#Reset()<CR>", { nnoremap = true })
-
+map("n", "<leader>m", ":MaximizerToggle!<CR>", { noremap = true })
+map("n", "<leader>dd", ":call vimspector#Launch()<CR>", { noremap = true })
+map("n", "<leader>de", ":call vimspector#Reset()<CR>", { noremap = true })
