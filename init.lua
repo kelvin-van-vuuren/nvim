97c2c6f29b3948faf201065f99b52cab84adfc40
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2022-10-20 15:41:43 +0100

    Remove Python3 provider
    Switched to nvim-dap from vimspector so python3 provider no longer needed

--- init.lua
+++ init.lua
@@ -32,13 +32,3 @@ augroup highlight_yank
     autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 80})
 augroup END
 ]])
-
--- enable python3 provider. needed for vimspector plugin.
-local enable_providers = {
-      "python3_provider",
-}
-
-for _, plugin in pairs(enable_providers) do
-    vim.g["loaded_" .. plugin] = nil
-    vim.cmd("runtime " .. plugin)
-end
