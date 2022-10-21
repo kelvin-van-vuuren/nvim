ccab88023845e5acd7ad1e1d895da5d51d082238
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2022-10-21 12:08:36 +0100

    Null-ls: move null-ls.lua under lsp folder

--- plugins/init.lua
+++ plugins/init.lua
@@ -59,7 +59,7 @@ return {
   ["jose-elias-alvarez/null-ls.nvim"] = {
     after = "nvim-lspconfig",
     config = function()
-      require "custom.plugins.null-ls"
+      require "custom.plugins.lsp.null-ls"
     end,
   },
 
