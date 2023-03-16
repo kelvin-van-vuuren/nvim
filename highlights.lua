7c092605a73478d59bf844a0fe32c3137be4fa3c
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-03-16 12:18:37 +0000

    Removing highlights for now
    Undercurl color change not working

--- highlights.lua
+++ highlights.lua
@@ -1,10 +1,10 @@
 local M = {}
 
-M.add_hlgroups = {
-	DiagnosticUnderlineError = { undercurl = true, fg = "red" },
-	DiagnosticUnderlineWarn = { undercurl = true, fg = "yellow" },
-	DiagnosticUnderlineInfo = { undercurl = true, fg = "green" },
-	DiagnosticUnderlineHint = { undercurl = true, fg = "purple" },
-}
+-- M.add_hlgroups = {
+-- 	DiagnosticUnderlineError = { undercurl = true, fg = "red" },
+-- 	DiagnosticUnderlineWarn = { undercurl = true, fg = "yellow" },
+-- 	DiagnosticUnderlineInfo = { undercurl = true, fg = "green" },
+-- 	DiagnosticUnderlineHint = { undercurl = true, fg = "purple" },
+-- }
 
 return M
