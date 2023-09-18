0e7c6eb05cb518a58441ad43cd3ac74f8cea70d0
Kelvin van Vuuren <kelvinvuuren@improbable.io>
2023-09-18 22:26:16 +0100

    Update highlights.lua
    Changes from updated upstream example config

--- highlights.lua
+++ highlights.lua
@@ -1,4 +1,20 @@
+-- To find any highlight groups: "<cmd> Telescope highlights"
+-- Each highlight group can take a table with variables fg, bg, bold, italic, etc
+-- base30 variable names can also be used as colors
+
 local M = {}
+
+---@type Base46HLGroupsList
+M.override = {
+  Comment = {
+    italic = true,
+  },
+}
+
+---@type HLTable
+M.add = {
+  NvimTreeOpenedFolderName = { fg = "green", bold = true },
+}
 
 -- M.add_hlgroups = {
 -- 	DiagnosticUnderlineError = { undercurl = true, fg = "red" },
