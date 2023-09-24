35aeddcd928a4f4dfa377d00d4de91672882189c
Kelvin van Vuuren <kelvinvuuren@improbable.io>
2023-09-24 17:51:56 +0100

    Plugin: github copilot

--- mappings.lua
+++ mappings.lua
@@ -164,4 +164,16 @@ M.plenary = {
 	},
 }
 
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+    }
+  }
+}
+
 return M
--- plugins.lua
+++ plugins.lua
@@ -121,6 +121,19 @@ return {
 		"vimwiki/vimwiki",
 	},
 
+	{
+		"github/copilot.vim",
+		lazy = false,
+		config = function()
+			-- Mapping tab is already used by NvChad
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_assume_mapped = true
+			vim.g.copilot_tab_fallback = ""
+			-- The mapping is set to other key, see custom/lua/mappings
+			-- or run <leader>ch to see copilot mapping section
+		end,
+	},
+
 	-- To make a plugin not be loaded
 	-- {
 	--   "NvChad/nvim-colorizer.lua",
