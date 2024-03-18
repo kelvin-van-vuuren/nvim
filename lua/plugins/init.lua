1a51cfda178967cc2d4131d8ca0441155959bf4b
Kelvin van Vuuren <kelvin.vanvuuren@skyral.io>
2024-03-18 12:01:01 +0000

    Replace copilot.vim with zbirenbaum/copilot.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -110,18 +110,55 @@ return {
 		"vimwiki/vimwiki",
 	},
 
-	{
-		"github/copilot.vim",
-		lazy = false,
-		config = function()
-			-- Mapping tab is already used by NvChad
-			vim.g.copilot_no_tab_map = true
-			vim.g.copilot_assume_mapped = true
-			vim.g.copilot_tab_fallback = ""
-			-- The mapping is set to other key, see custom/lua/mappings
-			-- or run <leader>ch to see copilot mapping section
-		end,
-	},
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = true,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>"
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+       })
+    end,
+  },
 
 	{
 		"leoluz/nvim-dap-go",
