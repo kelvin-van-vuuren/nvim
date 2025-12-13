191a75b500d0fc21603cc8a51a6ee3d4a4792bfd
Kelvin van Vuuren <kelvin.vanvuuren@skyral.io>
2025-12-13 13:35:24 +0000

    Fix git commit and highlight yank autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,22 +1,30 @@
 require("nvchad.autocmds")
 
--- highlight yank
-vim.cmd([[
-augroup highlight_yank
-    autocmd!
-    autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 80})
-augroup END
-]])
+-- highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
--- wrap git commit body message lines at 72 characters
-vim.cmd([["
-    augroup gitsetup
-        autocmd!
-        autocmd FileType gitcommit
-                \ autocmd CursorMoved,CursorMovedI * 
-                        \ let &l:textwidth = line('.') == 1 ? 50 : 72
-augroup end
-"]])
+-- git commit header 50 chars and wrap body message lines at 72 characters
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "gitcommit",
+	group = vim.api.nvim_create_augroup("GitCommitSetup", { clear = true }),
+	callback = function()
+		-- Create a buffer-local autocmd (note the buffer = 0)
+		-- This ensures the expensive CursorMoved check ONLY runs in this specific window
+		vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+			buffer = 0,
+			callback = function()
+				-- If on line 1, width is 50. Otherwise, it's 72.
+				vim.opt_local.textwidth = vim.fn.line(".") == 1 and 50 or 72
+			end,
+		})
+	end,
+})
 
 local enable_providers = {
 	"python3_provider",
